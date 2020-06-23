ccc3b4d2be981a98c76527c8ddeb32d0f12ee3fb
David Peter <dpet@mailbox.org>
2020-06-23 18:12:16 +0200

    Combine similar blocks

--- plot_net.py
+++ plot_net.py
@@ -44,29 +44,47 @@ if __name__ == '__main__':
     dim[2] //= first_conv["stride"][1]
     G.add_edge("L0", "L1", label="x".join([str(d) for d in dim]))
 
+    for i in reversed(range(len(net["blocks"]))):
+        if net["blocks"][i]["mobile_inverted_conv"]["name"] == "ZeroLayer":
+            del net["blocks"][i]
+
     layer = 1
-    for block in net["blocks"]:
-        prev_node = "L{}".format(layer-1)
+    blocks_per_node = 1
+    for i, block in enumerate(net["blocks"]):
         node = "L{}".format(layer)
-        next_node = "L{}".format(layer + 1)
+        next_node = "L{}".format(layer+1)
         conv = block["mobile_inverted_conv"]
+        if i < len(net["blocks"]) - 1:
+            next_conv = net["blocks"][i+1]["mobile_inverted_conv"]
+        else:
+            next_conv = None
         shortcut = block["shortcut"]
 
         if conv["name"] == "ZeroLayer":
             continue
         else:
             label = "MB{} {}x{}".format(conv["expand_ratio"], conv["kernel_size"], conv["kernel_size"])
+            if next_conv and next_conv["name"] != "ZeroLayer":
+                next_label = "MB{} {}x{}".format(next_conv["expand_ratio"], next_conv["kernel_size"], next_conv["kernel_size"])
+            else:
+                next_label = ""
 
-        G.add_node(node, label=label, color=colors[conv["kernel_size"]], width=shapes[conv["expand_ratio"]])
         dim[0] = conv["out_channels"]
         dim[1] //= conv["stride"]
         dim[2] //= conv["stride"]
-        G.add_edge(node, next_node, label="x".join([str(d) for d in dim]))
 
-        if shortcut and shortcut["name"] == "IdentityLayer":
-            G.add_edge(node, node, label="id")
+        if next_label == label:
+            blocks_per_node += 1
+        else:
+            print_label = "{} x ".format(blocks_per_node) + label
+            blocks_per_node = 1
+            G.add_node(node, label=print_label, color=colors[conv["kernel_size"]], width=shapes[conv["expand_ratio"]])
+            G.add_edge(node, next_node, label="x".join([str(d) for d in dim]))
 
-        layer += 1
+            #if shortcut and shortcut["name"] == "IdentityLayer":
+            #    G.add_edge(node, node, label="id")
+
+            layer += 1
 
     if verbose:
         # Feature mix layer
@@ -97,7 +115,7 @@ if __name__ == '__main__':
         # Classifier
         classifier = net["classifier"]
         conv_name = "FC" if classifier["name"] == "LinearLayer" else "Unknown"
-        G.add_node("L{}".format(layer), color="#8b8c8d", label="Pooling {}".format(conv_name))
+        G.add_node("L{}".format(layer), color="#8b8c8d", label="Pooling & {}".format(conv_name))
         dim[0] = classifier["out_features"]
         dim[1] = 1
         dim[2] = 1
